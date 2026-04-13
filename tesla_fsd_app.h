be0871451b7e70d3d52a6368abfe5b223fe51a65
hypery11 <hypery11@gmail.com>
2026-04-13 20:13:57 +0800

    v2.7.1: hotfix for MCP2515 SPI NULL crash

--- tesla_fsd_app.h
+++ tesla_fsd_app.h
@@ -11,7 +11,7 @@
 #include "libraries/mcp_can_2515.h"
 #include "fsd_logic/fsd_handler.h"
 
-#define TESLA_FSD_VERSION "2.7.0"
+#define TESLA_FSD_VERSION "2.7.1"
 
 typedef enum {
     TeslaFSDSceneMainMenu,
