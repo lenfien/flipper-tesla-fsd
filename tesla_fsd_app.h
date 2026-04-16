b641d5c54ba31ee33063513cc376db8df18456fe
hypery11 <hypery11@gmail.com>
2026-04-16 15:35:34 +0800

    v2.9.0: Ban Shield — freeze GTW_carConfig to block server-side VIN bans
    (cherry picked from commit 7eabefe7d8aeca6032b95308dbbb448e2bcf3f80)

--- tesla_fsd_app.h
+++ tesla_fsd_app.h
@@ -11,7 +11,7 @@
 #include "libraries/mcp_can_2515.h"
 #include "fsd_logic/fsd_handler.h"
 
-#define TESLA_FSD_VERSION "2.8.0"
+#define TESLA_FSD_VERSION "2.9.0"
 
 typedef enum {
     TeslaFSDSceneMainMenu,
